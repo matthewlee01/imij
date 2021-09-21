d75196215288884bd1311c1be0a4a3e66eff601a
Matthew Lee <laughew@gmail.com>
2021-09-20 22:35:51 -0700

    refactored and updated initial tests

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Image, :type => :model do 
 
+	# test image for the subject
 	test_file = Rails.root.join('spec', 'test.png')
-	test_image_file = ActiveStorage::Blob.create_and_upload!(
+
+	subject {
+		described_class.new(user_id: 1)
+	}
+
+	before {
+		subject.image_file.attach(
 			io: File.open(test_file, 'rb'),
 			filename: 'test.png',
-			content_type: 'image/png').signed_id
-	subject {
-		described_class.new(user_id: 1,
-												image_file: test_image_file)
+			content_type: 'image/png')
 	}
 	it "is valid with valid attributes" do 
 		expect(subject).to be_valid
+	end
+
+	it "is not valid without an image_file" do
+		subject.image_file = nil
+		expect(subject).to_not be_valid
 	end
 
 	it "is not valid without a user_id" do 
@@ -20,9 +29,7 @@ RSpec.describe Image, :type => :model do
 		expect(subject).to_not be_valid
 	end
 
-	it "is valid if image file is attached" do
-		subject.image_file = nil
-		expect(subject).to_not be_valid
+	it "properly attaches a file" do 
+		expect(subject.image_file).to be_attached
 	end
 end
-
